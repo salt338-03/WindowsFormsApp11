d65f5fcf171ba08ddbe37ac98e37eeb7b243b839
salt338-03 <salt338@naver.com>
2025-01-07 11:49:57 +0900

    초기 커밋.

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.progressBar1.Location = new System.Drawing.Point(106, 32);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(549, 23);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(490, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 54);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(106, 81);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(549, 23);
+            this.progressBar2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(106, 129);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(549, 23);
+            this.progressBar3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(106, 180);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(549, 23);
+            this.progressBar4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(106, 236);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(549, 23);
+            this.progressBar5.TabIndex = 13;
+            // 
+            // listBoxResults
+            // 
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.ItemHeight = 12;
+            this.listBoxResults.Location = new System.Drawing.Point(64, 294);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(210, 88);
+            this.listBoxResults.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 450);
+            this.Controls.Add(this.listBoxResults);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.ListBox listBoxResults;
+    }
+}
+
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private Thread[] progressThread;
+        private ProgressBar[] ProgressBar;
+        private bool[] isFinished; // 차량의 완료 상태를 추적
+        private bool isRunning;
+        private DateTime startTime;
+        public Form1()
+        {
+            InitializeComponent();
+            ProgressBar = new ProgressBar[] { progressBar1, progressBar2, progressBar3, progressBar4, progressBar5 };
+            progressThread = new Thread[ProgressBar.Length];
+            isFinished = new bool[ProgressBar.Length];
+
+            for (int i = 0; i < ProgressBar.Length; i++)
+            {
+                ProgressBar[i].Minimum = 0;
+                ProgressBar[i].Maximum = 100;
+                ProgressBar[i].Value = 0;
+                isFinished[i] = false;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isRunning) return;
+
+            isRunning = true;
+            startTime = DateTime.Now;
+            for (int i = 0; i < progressThread.Length; i++)
+            {
+                int index = i;
+                progressThread[i] = new Thread(() => UpdateProgressBar(index));
+                progressThread[i].IsBackground = true;
+                progressThread[i].Start();
+            }
+        }
+        private void UpdateProgressBar(int index)
+        {
+            while (isRunning)
+            {
+               
+                int newValue = ProgressBar[index].Value + 1; 
+                newValue = Math.Min(ProgressBar[index].Maximum, newValue); //100넘지않게
+
+                // UI 스레드에서 ProgressBar 값 업데이트
+                Invoke(new MethodInvoker(delegate ()
+                {
+                    ProgressBar[index].Value = newValue;
+                }));
+
+
+
+                if (newValue >= ProgressBar[index].Maximum && !isFinished[index])
+                {
+                    isFinished[index] = true; 
+
+                   
+                    TimeSpan elapsedTime = DateTime.Now - startTime;
+                    string carName = $"Car {index + 1}";
+
+
+                    Invoke(new MethodInvoker(delegate ()
+                    {
+                        listBoxResults.Items.Add($"{carName} finished in {elapsedTime.TotalSeconds:F2} seconds");
+                    }));
+
+
+
+                    if (Array.TrueForAll(isFinished, finished => finished))
+                    {
+                        isRunning = false; 
+                        MessageBox.Show("레이스 종료!");
+                    }
+                }
+                Thread.Sleep(random.Next(1, 100));
+            }
+        }
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
